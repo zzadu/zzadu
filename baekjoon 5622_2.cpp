7a977b4fe91c2211b202d04f2b669b919c48b25a
zzadu <76769078+zzadu@users.noreply.github.com>
2021-01-06 17:05:48 +0900

    Update baekjoon 5622_2.cpp

--- baekjoon 5622_2.cpp
+++ baekjoon 5622_2.cpp
@@ -2,10 +2,6 @@
 #include <string>
 using namespace std;
 
-int convert(char a, int arr[]) {
-    return arr[a - 'A'];
-}
-
 int main() {
     string number;
     int alphaNum[26] = { 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9 };
@@ -15,7 +11,7 @@ int main() {
     int size = number.length() / sizeof(char);
     int sum = 0;
 
-    for (int i = 0; i < size; i++) sum += convert(number[i], alphaNum) + 1;
+    for (int i = 0; i < size; i++) sum += alphaNum[number[i] - 'A'] + 1;
 
     cout << sum << "\n";
 
